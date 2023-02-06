c197673c82f83042fc47b6e0c17cf7d60ed6e65a
Mauro Solcia <mauro.solcia@gmail.com>
2023-02-06 20:09:07 +0100

    mmm

--- Bluetti_ESP32/Bluetti_ESP32.ino
+++ Bluetti_ESP32/Bluetti_ESP32.ino
@@ -32,7 +32,7 @@ void setup()
   initBWifi(false); // Init Wifi and WebServer
   writeLog("---------------------------");
   writeLog("Starting");
-  // initBluetooth();
+  initBluetooth();
 
   esp_task_wdt_init(WDT_TIMEOUT, true); // enable panic so ESP32 restarts
   esp_task_wdt_add(NULL);               // add current thread to WDT watch
