4dd18ce62bdd1019f31920d190f18b9e33faed4b
Mauro Solcia <mauro.solcia@gmail.com>
2024-11-23 18:04:05 +0100

    wifi timeout on disconnect 15 minutes then start in ap mode

--- Bluetti_ESP32/BWifi.cpp
+++ Bluetti_ESP32/BWifi.cpp
@@ -1116,12 +1116,12 @@ void wifiConnect(bool resetWifi)
     {
       delay(500);
       Serial.print(F("."));
-      if ((millis() - connectTimeout) > (5 * 60 * 1000))
+      if ((millis() - connectTimeout) > (15 * 60 * 1000))
       {
         Serial.println("Wifi Not connected with ssid: " + wifiConfig.ssid + " ! Force AP Mode");
         // writeLog("Wifi Not connected with ssid: " + wifiConfig.ssid + " ! Force AP Mode");
-        // wifiConfig.APMode = true;
-        // saveConfig();
+        wifiConfig.APMode = true;
+        saveConfig();
         _rebootDevice = true; // reboot the device, but keep trying to reconnect this should avoid battery drain if the wifi goes down
         break;
       }
